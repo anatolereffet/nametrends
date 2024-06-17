4fef462427fbec6730b0606f40878bde1eff3326
anatolereffet <anatole.reffet@ieseg.fr>
2024-06-17 11:04:53 +0200

    update initial basic filter and add requirements.txt

--- src/process_data.py
+++ src/process_data.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 df = (
     pd.read_csv("./data/dpt2020.csv", sep=";")
-    .loc[lambda df: df["preusuel"] != "_PRENOMS_RARES"]
-    .loc[lambda df: df["annais"] != "XXXX"]
-    .sort_values("annais")
+    .rename(columns={"annais": "annee", "preusuel": "prenoms"})
+    .loc[lambda df: df["prenoms"] != "_PRENOMS_RARES"]
+    .loc[lambda df: df["annee"] != "XXXX"]
+    .loc[lambda df: df["dpt"] != "XX"]
+    .sort_values("annee")
     .drop("sexe", axis=1)
 )
 

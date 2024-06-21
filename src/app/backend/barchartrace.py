33e3476ae5e3e5eeb5db3afe0205719d1cd47bb1
anatolereffet <anatole.reffet@ieseg.fr>
2024-06-21 16:34:48 +0200

    feat: add male/female bar chart race, needs cleanup on latency/code

--- src/app/backend/barchartrace.py
+++ src/app/backend/barchartrace.py
@@ -5,19 +5,46 @@ from raceplotly.plots import barplot
 def bar_chart_race_callbacks(app):
     @app.callback(
         Output("graph-raceplot", "figure"),
-        [Input("url", "pathname")]
+        Output("graph-raceplot_male", "figure"),
+        Output("graph-raceplot_female", "figure"),
+        [Input("url", "pathname")],
     )
     def create_bar_chart_race(pathname):
         data = app.df
-        grouped_df = data.groupby(['prenoms', 'annee'], as_index=False).sum()
+        grouped_df = data.groupby(["prenoms", "annee", "sexe"], as_index=False).agg(
+            {"nombre": "sum"}
+        )
+        print(grouped_df)
+        grouped_male = grouped_df.loc[grouped_df["sexe"] == 1]
+        print(grouped_male)
+        grouped_female = grouped_df.loc[grouped_df["sexe"] == 2]
         # Create bar plot race
-        my_raceplot = barplot(grouped_df,
-                              item_column='prenoms',
-                              value_column='nombre',
-                              time_column='annee')
+        my_raceplot = barplot(
+            grouped_df,
+            item_column="prenoms",
+            value_column="nombre",
+            time_column="annee",
+        )
+        raceplot_m = barplot(
+            grouped_male,
+            item_column="prenoms",
+            value_column="nombre",
+            time_column="annee",
+        )
+        raceplot_f = barplot(
+            grouped_female,
+            item_column="prenoms",
+            value_column="nombre",
+            time_column="annee",
+        )
         # Generate the race plot figure
-        fig = my_raceplot.plot(title='Top 10 prenoms',
-                               item_label='Top 10 prenoms',
-                               value_label='nombre',
-                               frame_duration=1000)
-        return fig
+        fig = my_raceplot.plot(
+            title="Top 10 names global scale", value_label="number", frame_duration=1000
+        )
+        male_fig = raceplot_m.plot(
+            title="Top 10 male names", value_label="number", frame_duration=1000
+        )
+        female_fig = raceplot_f.plot(
+            title="Top 10 female names", value_label="number", frame_duration=1000
+        )
+        return fig, male_fig, female_fig
--- src/app/frontend/pages/barchartrace.py
+++ src/app/frontend/pages/barchartrace.py
@@ -4,9 +4,11 @@ import dash_bootstrap_components as dbc
 
 
 def create_layout(app):
-    return html.Div([
-        html.H1("Bar Chart Race for Top 10 Prenoms"),
-        dcc.Graph(
-            id='graph-raceplot'
-        )
-    ])
+    return html.Div(
+        [
+            html.H1("Bar Chart Race for Top 10 Prenoms"),
+            dcc.Graph(id="graph-raceplot"),
+            dcc.Graph(id="graph-raceplot_male"),
+            dcc.Graph(id="graph-raceplot_female"),
+        ]
+    )

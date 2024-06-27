93e9209adf7d326bb497effb704388d9073b7f46
anatolereffet <anatole.reffet@ieseg.fr>
2024-06-27 17:27:03 +0200

    fix: drop txt dump in repo, update makefile & requirements

--- Makefile
+++ Makefile
@@ -12,6 +12,7 @@ pull_data:
 	@test -e $(DIR_PATH)/data/dpt2020.csv || wget https://perso.telecom-paristech.fr/eagan/class/igr204/data/dpt2020.csv -P $(DIR_PATH)/data
 	@test -e $(DIR_PATH)/data/$(DPT_FILE) || wget -O $(DIR_PATH)/data/$(DPT_FILE) https://raw.githubusercontent.com/Kaosamami/france-geojson/main/departement_avec_outremer_rapprochée.geojson
 	@test -e $(DIR_PATH)/data/$(REGION_FILE) || wget -O $(DIR_PATH)/data/$(REGION_FILE) https://raw.githubusercontent.com/Kaosamami/france-geojson/main/region_avec_outremer_rapprochée.geojson
+	@test -e $(DIR_PATH)/data/names.txt || @$(PYTHON_INTERPRETER) src/scrapper.py
 
 clean_data:
 	@$(PYTHON_INTERPRETER) src/process_data.py
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,11 +1,15 @@
 import requests
 import time
 from bs4 import BeautifulSoup
+import string
+import os
+from unidecode import unidecode
 
 
 def main():
     # URL of the webpage to scrape
-    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    for letter in string.ascii_uppercase:
+        # Avoid overloading server with requests
         time.sleep(3)
         url = f"https://nominis.cef.fr/contenus/prenom/biblique/{letter}.html"
 
@@ -18,25 +22,23 @@ def main():
             # Parse the content of the page
             soup = BeautifulSoup(response.content, "html.parser")
             # Find all h5 elements with the class 'mb1'
-            links = soup.find_all(
-                "a", class_="list-group-item list-group-item-action")
+            links = soup.find_all("a", class_="list-group-item list-group-item-action")
 
             # Extract the text from the 'h5' elements within these 'a' elements
             name_list = []
             for link in links:
                 h5_tag = link.find("h5", class_="mb-1")
                 if h5_tag:
-                    name_list.append(h5_tag.get_text(strip=True))
+                    name_list.append(unidecode(h5_tag.get_text(strip=True)).upper())
 
             # Print the extracted names
             for name in name_list:
                 print(name)
                 # save the names in a file
-                with open("names.txt", "a") as file:
+                with open(os.path.join("data", "names.txt"), "a") as file:
                     file.write(name + "\n")
         else:
-            print("Failed to retrieve the webpage. Status code:",
-                  response.status_code)
+            print("Failed to retrieve the webpage. Status code:", response.status_code)
 
 
 if __name__ == "__main__":
